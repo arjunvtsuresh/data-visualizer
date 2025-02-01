36e4aa5d899e2550a67c720bdff19846e2d1a636
Arjun Suresh <arjunsuresh@Arjuns-MacBook-Pro.local>
2025-02-01 14:14:19 +0530

    add UI and data

---
+++ dashboard/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Card from './components/ui/Card';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello World</h1>
+        <Card></Card>
+        <Card></Card>
+    </div>
+  );
+}
+
+export default App;
